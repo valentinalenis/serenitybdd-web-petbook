33ef90aab4e89739a3a70bd9fd297258b2ab91a5
valentinalenis <32966903+valentinalenis@users.noreply.github.com>
2020-08-31 06:55:29 -0500

    first commit

---
+++ src/test/java/co/com/devco/runners/GoogleSearchRunner.java
@@ -0,0 +1,16 @@
+package co.com.devco.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+		features = "src/test/resources/features/google_search.feature",
+		glue = {"co.com.devco.stepdefinitions"},
+		snippets = SnippetType.CAMELCASE
+)
+public class GoogleSearchRunner {
+
+}
